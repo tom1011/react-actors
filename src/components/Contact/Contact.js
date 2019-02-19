93a23c5e5645a6f3f2e7788137084956b19644d0
David Friday <davidsfriday13@gmail.com>
2019-02-19 17:25:24 -0600

    basic done

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+      <h2>Contact </h2>
+      <p>Phone 1-800-WE-REACT</p>
+
+      <p>Email: info@ReactActors.com</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
